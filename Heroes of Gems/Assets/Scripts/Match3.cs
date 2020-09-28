2eaf5d10bc939f843b0f6108a4a7c64494baf682
Bálint Köles <koles.balint@gmail.com>
2020-09-28 13:35:04 +0200

    add script prototype

---
+++ Heroes of Gems/Assets/Scripts/Match3.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match3 : MonoBehaviour
+{
+
+    public ArrayLayout boardLayout;
+    int width = 9;
+    int height = 14;
+    Node[,] board;
+
+    System.Random random;
+    void Start()
+    {
+
+    }
+
+    void StartGame()
+    {
+        board = new Node[width, height];
+        string seed = getRandomSeed();
+        random = new System.Random(seed.GetHashCode());
+
+        InitializeBoard();
+    }
+
+    void InitializeBoard()
+    {
+        board = new Node[width, height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x > width; x++)
+            {
+                board[x, y] = new Node(-1, new Point(x, y));
+            }
+        }
+    }
+    void Update()
+    {
+
+    }
+
+    string getRandomSeed()
+    {
+        string seed = "";
+        string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+        for (int i = 0; i < 20; i++)
+        {
+            seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+        }
+
+        return seed;
+    }
+}
+
+[System.Serializable]
+public class Node
+{
+    public int value; //Az adott mezőn található Gem értéke
+    public Point index;
+
+    public Node(int v, Point i)
+    {
+        value = v;
+        index = i;
+    }
+
+}
---
+++ Heroes of Gems/Assets/Scripts/Point.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Point
+{
+    public int x;
+    public int y;
+
+    public Point(int nx, int ny)
+    {
+        x = nx;
+        y = ny;
+    }
+
+    public Vector2 ToVector()
+    {
+        return new Vector2(x, y);
+    }
+
+    public bool Equals(Point point)
+    {
+        return (x == point.x && y == point.y);
+    }
+    public void mul(int szorzo)
+    {
+        x *= szorzo;
+        y *= szorzo;
+    }
+    public void add(Point point)
+    {
+        x += point.x;
+        y += point.y;
+    }
+
+    public static Point fromVector(Vector2 v)
+    {
+        return new Point((int)v.x, (int)v.y);
+    }
+    public static Point fromVector(Vector3 v)
+    {
+        return new Point((int)v.x, (int)v.y);
+    }
+
+    public static Point mul(Point point, int szorzo)
+    {
+        return new Point(point.x * szorzo, point.y * szorzo);
+    }
+    public static Point add(Point point, Point point2)
+    {
+        return new Point(point.x + point2.x, point.y + point2.y);
+    }
+
+    public static Point zero
+    {
+        get { return new Point(0, 0); }
+    }
+    public static Point one
+    {
+        get { return new Point(1, 1); }
+    }
+    public static Point up
+    {
+        get { return new Point(0, 1); }
+    }
+    public static Point down
+    {
+        get { return new Point(0, -1); }
+    }
+    public static Point left
+    {
+        get { return new Point(-1, 0); }
+    }
+    public static Point right
+    {
+        get { return new Point(1, 0); }
+    }
+
+}
