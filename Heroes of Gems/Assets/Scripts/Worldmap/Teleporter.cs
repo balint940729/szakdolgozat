87be40ecc8a8e2941c2fd4e247e24322df2f031b
Bálint Köles <koles.balint@gmail.com>
2020-12-08 03:08:58 +0100

    Add Astar AI

--- Heroes of Gems/Assets/Scripts/Worldmap/Teleporter.cs
+++ Heroes of Gems/Assets/Scripts/Worldmap/Teleporter.cs
@@ -7,14 +7,16 @@ public class Teleporter : MonoBehaviour
     public bool teleported = false;
     public Teleporter destination;
     public GameObject thePlayer;
+    public GameObject theFollower;
     public Animator animator;
+    Vector3 followerOffset;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
             if (!teleported)
             {
                 animator.SetBool("StartTeleport", true);
-                StartCoroutine(Teleport(thePlayer)); 
+                StartCoroutine(Teleport(thePlayer));              
             }
     }
 
@@ -22,9 +24,13 @@ public class Teleporter : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
         destination.teleported = true;
+        followerOffset.x = -1f;
+        followerOffset.y = 1f;
+        followerOffset.z = 1f;
 
         Camera.main.transform.position += destination.gameObject.transform.position - go.transform.position;
         go.transform.position = destination.gameObject.transform.position;
+        theFollower.transform.position = destination.gameObject.transform.position + followerOffset;
         animator.SetBool("StartTeleport", false);
         
     }
