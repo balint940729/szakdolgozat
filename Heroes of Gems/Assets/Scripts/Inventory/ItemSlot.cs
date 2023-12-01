e9bfec3c5b1fe564b715d2286a7c997eb8eaddff
Bálint Köles <koles.balint@gmail.com>
2023-12-01 14:03:07 +0100

    Add inventory

--- Heroes of Gems/Assets/Scripts/Inventory/ItemSlot.cs
+++ Heroes of Gems/Assets/Scripts/Inventory/ItemSlot.cs
@@ -1,27 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour {
-    //[SerializeField] private Image image;
+    [SerializeField] private Image image;
 
-    //private Item item;
+    private Item item;
 
-    //public Item Item {
-    //    get { return item; }
-    //    set {
-    //        item = value;
-    //        if (item == null) {
-    //            image.enabled = false;
-    //        }
-    //        else {
-    //            image.sprite = item.Icon;
-    //            image.enabled = true;
-    //        }
-    //    }
-    //}
+    public Item Item {
+        get { return item; }
+        set {
+            item = value;
+            if (item == null) {
+                image.enabled = false;
+            }
+            else {
+                image.sprite = item.Icon;
+                image.enabled = true;
+            }
+        }
+    }
 
-    //private void OnValidate() {
-    //    if (image == null) {
-    //        image = GetComponent<Image>();
-    //    }
-    //}
+    private void OnValidate() {
+        if (image == null) {
+            image = GetComponent<Image>();
+        }
+    }
 }
